fc77486f80d6cb41f3c4d85a100850e713ba3a27
AsmaelShowky70 <asmaelshoky70@gmail.com>
2023-07-16 11:10:30 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
         primarySwatch: Colors.teal,
         canvasColor: Colors.black,
         //  useMaterial3: false,
